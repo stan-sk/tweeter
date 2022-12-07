09e203d34136d136a8ec0a3b2adbfbc72d5edab4
Stanley Lai <stanley.lai@telus.com>
2022-12-06 16:14:47 -0800

    Implement renderTweets function with hardcoded array data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,58 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-const tweetData = {
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1670187449475
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
+// const tweetData = {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1670187449475
+// }
 
 const createTweetElement = function (object) {
-const markup = `
+  let $tweet = `
       <section class="all-tweets" id="tweets-container">
         <article>
           <header class="tweet-header">
-            <div class="tweeter"><img class ="avatar" src=${tweetData.user.avatars}>
-            <h5 class="tweeterName">${tweetData.user.name}</h5></div>
-            <div class="username"><h5>${tweetData.user.handle}</h5></div>
+            <div class="tweeter"><img class ="avatar" src=${object.user.avatars}>
+            <h5 class="tweeterName">${object.user.name}</h5></div>
+            <div class="username"><h5>${object.user.handle}</h5></div>
           </header>
           <article class="article">
-          ${tweetData.content.text}
+          ${object.content.text}
           </article>
           <hr>
           <footer class="footer">
-            <span><h5>${tweetData.created_at}</h5></span>
+            <span><h5>${object.created_at}</h5></span>
             <div>
             <i class="icon fa-solid fa-flag"></i>
             <i class="icon fa-sharp fa-solid fa-retweet"></i>
@@ -41,12 +64,20 @@ const markup = `
           </footer>
         </article>
       </section>
-`
-return markup
+    `
+  return $tweet
 }
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
 
-$(document).ready(function() {
-  $("#tweets-container").after($tweet);
-});
+    $(document).ready(function() {
+      $("#tweets-container").after($tweet);
+    });
+  }
+  return;
+}
+
+
+renderTweets(data)
