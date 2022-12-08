b3548b9486bd8a36066b0343d6819034bb999c02
Stanley Lai <stanley.lai@telus.com>
2022-12-07 20:21:18 -0800

    added CSS/ redirect to textarea when clicking write a new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-// const tweetData = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1670187449475
-// }
-
 const escapeFn = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -91,16 +54,15 @@ const loadTweets = function () {
       renderTweets(tweet);
     }
   })
-  // $.ajax("/tweets", { method: "GET"})
-  // .then (function (tweet) {
-  //   renderTweets(tweet)
-  //   return;
-  // })
 }
 
 $(document).ready(function() {
 
   loadTweets();
+
+  $("div.newTweet").click(function() {
+    $("#tweet-text").focus();
+  });
 
   $('.tweetform').submit(function(event) {
     event.preventDefault(); 
@@ -124,3 +86,5 @@ $(document).ready(function() {
     this.reset();
   })
 });
+
+
